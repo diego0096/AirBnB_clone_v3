5ab3b91d74f5f505267cae3f1f4516a31d037b09
dfbq91 <dfbq91@gmail.com>
2020-04-29 14:32:48 -0500

    changed logic to test why does it failing

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,20 +3,26 @@
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
+classes = {"users": "User", "places": "Place",
+            "states": "State", "cities": "City",
+            "amenities": "Amenity", "reviews": "Review"}
 
-
-@app_views.route('/status', methods=['GET'])
+@app_views.route('/status')
 def status():
     '''Return status (json format) of the object app_views'''
     return jsonify({'status': 'OK'})
 
 
-@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+@app_views.route('/stats')
 def stats():
     """ Returns the number of each instance type """
-    return jsonify(amenities=storage.count("Amenity"),
-                   cities=storage.count("City"),
-                   places=storage.count("Place"),
-                   reviews=storage.count("Review"),
-                   states=storage.count("State"),
-                   users=storage.count("User"))
+    # return jsonify(amenities=storage.count("Amenity"),
+    #                cities=storage.count("City"),
+    #                places=storage.count("Place"),
+    #                reviews=storage.count("Review"),
+    #                states=storage.count("State"),
+    #                users=storage.count("User"))
+    instance_count = {}
+    for key, value in classes.items():
+        instance_count[key] = storage.count(value)
+    return jsonify(instance_count)
