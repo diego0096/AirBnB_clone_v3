3df0f8d171fef2311098af141f073139ed8bd1f6
dfbq91 <dfbq91@gmail.com>
2020-04-29 14:39:09 -0500

    come back, the code doesnt work

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,7 @@
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
-classes = {"users": "User", "places": "Place",
-            "states": "State", "cities": "City",
-            "amenities": "Amenity", "reviews": "Review"}
+
 
 @app_views.route('/status')
 def status():
@@ -13,16 +11,12 @@ def status():
     return jsonify({'status': 'OK'})
 
 
-@app_views.route('/stats')
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def stats():
     """ Returns the number of each instance type """
-    # return jsonify(amenities=storage.count("Amenity"),
-    #                cities=storage.count("City"),
-    #                places=storage.count("Place"),
-    #                reviews=storage.count("Review"),
-    #                states=storage.count("State"),
-    #                users=storage.count("User"))
-    instance_count = {}
-    for key, value in classes.items():
-        instance_count[key] = storage.count(value)
-    return jsonify(instance_count)
+    return jsonify(amenities=storage.count("Amenity"),
+                   cities=storage.count("City"),
+                   places=storage.count("Place"),
+                   reviews=storage.count("Review"),
+                   states=storage.count("State"),
+                   users=storage.count("User"))
